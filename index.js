51530e3eb8f95ccd15374677ef62db92723b3d03
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-08-11 12:57:18 +0530

    Add home.ejs file

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 const express = require("express");
-const {connectToMongoDB} =require("./connect");
+const path = require("path");
+const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
-const URL = require('./models/url');
+const URL = require("./models/url");
+
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
     .then(() => console.log("MongoDB connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 
-app.get('/:shortId', async (req ,res) => {
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+
+    return res.render("home", {
+        urls: allUrls,
+    });
+});
+
+app.get("/url:shortId", async (req, res) => {
     const shortId = req.params.shortId;
-const entry = await URL.findOneAndUpdate({
-        shortId
-    }, {$push: {
-        visitHistory: {
-            timestamp: Date.now(),
+
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
         }
-    }})
-    res.redirect(entry.redirectURL)
-})
+    );
+
+    if (!entry) {
+        return res.status(404).send("Short URL not found");
+    }
+    res.redirect(entry.redirectURL);
+});
 
 app.use("/url", urlRoute);
 
-app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Server Started at PORT ${PORT}`);
+});
