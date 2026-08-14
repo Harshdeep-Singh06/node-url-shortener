5d763c3acf7c2699dc80bc1a6e73803ac0b7b0d6
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-08-14 15:12:37 +0530

    Building Node.js Authentication from Scratch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const { connectToMongoDB } = require("./connect");
+const cookieParser = require('cookie-parser');
+const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth');
 
 const URL = require("./models/url");
 
@@ -20,6 +22,9 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+
 
 app.get("/test", async (req, res) => {
     const allUrls = await URL.find({});
@@ -50,9 +55,9 @@ app.get("/url/:shortId", async (req, res) => {
     res.redirect(entry.redirectURL);
 });
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedinUserOnly,urlRoute);
 app.use("/user", userRoute);
-app.use("/", staticRoute);
+app.use("/",checkAuth, staticRoute);
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT ${PORT}`);
